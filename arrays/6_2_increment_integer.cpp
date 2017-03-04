4e9181a1550882528531fc3df6508bb0729cf3c2
spookyQubit <iitkgp.shantanu@gmail.com>
2017-03-04 16:26:05 -0500

    increment arbitrary precision integer represented as vactor

---
+++ arrays/6_2_increment_integer.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+/*
+Purpose: Increment an arbitrary precision integer represented in a vector. 
+For example 145 is represented as <1, 4, 5> and the incremented solution should be <1, 4, 6>
+*/
+
+void printArray(const std::vector<int>& ar)
+{
+	for(std::vector<int>::const_iterator iter = ar.begin();
+		iter != ar.end();
+		++iter)
+	{
+		std::cout << *iter << " ";
+	}
+	std::cout << std::endl;
+}
+
+void reverseArray(std::vector<int>* pt_reverse_in_ar, 
+	              const std::vector<int>& in_ar)
+{
+	for(int i = in_ar.size() - 1; i >=0; --i)
+	{
+		pt_reverse_in_ar->push_back(in_ar[i]);
+	}
+}
+
+void incrementInteger(std::vector<int>* pt_result,
+	                  const std::vector<int>& in_ar,
+	                  int d)
+{
+	std::vector<int>& result = *pt_result;
+	std::vector<int> reverse_result;
+    
+    std::vector<int> reverse_in_ar;
+	reverseArray(&reverse_in_ar, in_ar);
+
+ 
+	int div = 0;
+	for(int i = 0; i < reverse_in_ar.size(); ++i)
+	{
+		int current = div + reverse_in_ar[i] + d;
+		int remainder = current % 10;
+		div = current / 10;
+		d = 0; // Do not add the number after the first time.
+
+		reverse_result.push_back(remainder);
+	}
+
+	if(div != 0)
+	{
+		reverse_result.push_back(div);
+	}
+
+	reverseArray(&result, reverse_result);
+}
+
+int main()
+{
+	std::vector<int> ar = {9, 9};
+    std::vector<int> result;
+    int d = 1;
+
+    incrementInteger(&result, ar, d); 
+
+    printArray(result);
+}
