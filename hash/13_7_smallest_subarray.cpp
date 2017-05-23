3264bca6fa5769a2fd736999c640498dc0e48ca3
spookyQubit <iitkgp.shantanu@gmail.com>
2017-05-23 18:23:13 -0400

    cleaner smallest subarray

--- hash/13_7_smallest_subarray.cpp
+++ hash/13_7_smallest_subarray.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <unordered_set>
 #include <unordered_map>
+#include <limits>
 
 using namespace std;
 
@@ -13,47 +14,44 @@ pair<int, int> get_smallest_subarray(const vector<string> &a, const unordered_se
 {
 	pair<int, int> result = make_pair(-1, -1);
 
-	unordered_map<string, int> count_of_words_in_current_subarray;
-	// Starting at 0, find the last index in array which contains all the words. 
 	int min_distance = a.size() + 1;
+	unordered_map<string, int> word_counts_in_current_subarray;	
 	int l = 0;
 	int r = 0;
-	for( ; l<a.size(); ++l){
-		for( ; r<a.size(); ++r)
-		{
+	while(l < a.size() && r < a.size()){
+		while(r<a.size() && word_counts_in_current_subarray.size() < words.size()){
 			if(words.find(a[r]) != words.end()){
-				if(count_of_words_in_current_subarray.find(a[r]) != count_of_words_in_current_subarray.end()){
-					count_of_words_in_current_subarray[a[r]] += 1;
+				if(word_counts_in_current_subarray.find(a[r]) != word_counts_in_current_subarray.end()){
+					word_counts_in_current_subarray[a[r]] += 1;
 				}else{
-					count_of_words_in_current_subarray.emplace(a[r], 1);
+					word_counts_in_current_subarray.emplace(a[r], 1);
 				}
 			}
-
-			if(count_of_words_in_current_subarray.size() == words.size()){
-				if(r - l < min_distance){
-					min_distance = r - l;
-					result.first = l;
-					result.second = r;
-				}
-				break;
-			}
+			++r;
 		}
 
-		if(count_of_words_in_current_subarray.find(a[l]) != count_of_words_in_current_subarray.end()){
-			--count_of_words_in_current_subarray[a[l]];
-			if(count_of_words_in_current_subarray[a[l]] == 0){
-				count_of_words_in_current_subarray.erase(a[l]);
+		if(r - l < min_distance && word_counts_in_current_subarray.size() == words.size()){
+			min_distance = r - l;
+			result.first = l;
+			result.second = r - 1;
+		}
+
+		cout << "l = " << l << endl;
+		if(words.find(a[l]) != words.end()){
+			word_counts_in_current_subarray[a[l]] -= 1;
+			if(word_counts_in_current_subarray[a[l]] == 0){
+				word_counts_in_current_subarray.erase(a[l]);
 			}
 		}
+		++l;
 	}
-	
 	return result;
 }
 
 void test1()
 {
 	vector<string> a = {"apple", "banana", "dog", "pineapple", "orrange", "cat", "mango"};
-	unordered_set<string> words = {"banana", "orrange", "blah"};
+	unordered_set<string> words = {"banana", "orrange"};
 
 	pair<int, int> indexes = get_smallest_subarray(a, words);
 	cout << "(" << indexes.first << ", " << indexes.second << ")" << endl;
